d17928fe30b4d166f9ebaefccdc5fcc355528bbf
Joynal Abedin <cfcfahim26@gmail.com>
2024-05-03 11:15:13 +0600

    Provders and wrappers for db added

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,18 +1,82 @@
-export const fetchWrapper = async (url: string, options: RequestInit) => {
+import { GraphQLFormattedError } from 'graphql';
+
+type Error = {
+  message: string;
+  statusCode: string;
+};
+
+/**
+ * Sends a custom fetch request to the specified URL with the given options.
+ *
+ * @param {string} url - The URL to send the request to.
+ * @param {RequestInit} options - The options for the fetch request.
+ * @returns {Promise<any>} - A promise that resolves to the JSON response from the server.
+ * @throws {Error} - If the response is not OK, an error with the response text is thrown.
+ */
+
+const customFetch = async (url: string, options: RequestInit) => {
   const accessToken = localStorage.getItem('accessToken');
   const headers = options.headers as Record<string, string>;
 
-  const res = await fetch(url, {
+  return await fetch(url, {
     ...options,
     headers: {
       ...headers,
       Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      'Apollo-Require-Preflight': 'true',
     },
   });
+};
 
-  if (!res.ok) {
-    const error = await res.text();
-    throw new Error(error);
+/**
+ * Retrieves GraphQL errors from the response body.
+ *
+ * @param body - The response body containing the GraphQL errors.
+ * @returns An Error object if there are GraphQL errors in the body, otherwise null.
+ */
+const getGraphQLErrors = (
+  body: Record<'errors', GraphQLFormattedError[] | undefined>
+): Error | null => {
+  if (!body) {
+    return {
+      message: 'Unknown error',
+      statusCode: 'INTERNAL_SERVER_ERROR',
+    };
   }
-  return await res.json();
+
+  if ('errors' in body) {
+    const errors = body?.errors;
+    const messages = errors?.map((error) => error?.message)?.join('');
+    const code = errors?.[0]?.extensions?.code;
+
+    return {
+      message: messages || JSON.stringify(errors),
+      statusCode: code || 500,
+    };
+  }
+
+  return null;
+};
+
+/**
+ * Wrapper function for making HTTP requests using customFetch.
+ *
+ * @param {string} url - The URL to make the request to.
+ * @param {RequestInit} options - The options for the request.
+ * @returns {Promise<any>} - A promise that resolves to the response body.
+ * @throws {Error} - If there is an error in the GraphQL response.
+ */
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
+
+  const responseClone = await response.clone(); // Clone the response to read it twice if needed
+  const body = await response.json();
+
+  const error = getGraphQLErrors(body);
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return response;
 };
--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,8 +1,20 @@
-import { GraphQLClient } from '@refinedev/nestjs-query';
+import graphqlDataProvider, {
+  GraphQLClient,
+  liveProvider as graphqlLiveProvider,
+} from '@refinedev/nestjs-query';
 import { fetchWrapper } from './fetch-wrapper';
+import { createClient } from 'graphql-ws';
 
+export const BASE_URL = 'https://api.crm.refine.dev';
 export const API_URL = 'https://api.crm.refine.dev';
-
+export const WS_URL = 'wss://api.crm.refine.dev/graphql';
+/**
+ * Creates a new instance of GraphQLClient with a custom fetch function.
+ *
+ * @param {string} API_URL - The URL of the GraphQL API.
+ * @param {function} fetch - The custom fetch function to be used for making requests.
+ * @returns {GraphQLClient} - The new instance of GraphQLClient.
+ */
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
     try {
@@ -12,3 +24,31 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+/**
+ * Initializes a WebSocket client if the code is running in a browser environment.
+ * The client is created with the specified WebSocket URL and connection parameters,
+ * including the access token retrieved from the local storage.
+ *
+ * @returns {WebSocketClient | undefined} The WebSocket client if running in a browser environment, otherwise undefined.
+ */
+const wsClient =
+  typeof window !== 'undefined'
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem('access_token');
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+// The liveProvider is only available in the browser environment. And We can subscribe to the live queries and mutations.
+export const liveProvider = wsClient
+  ? graphqlLiveProvider(wsClient)
+  : undefined;
